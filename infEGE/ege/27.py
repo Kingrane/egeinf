8fd4d38f5926ad23e1e7af501be0b1c5e8cd8b23
kingrane <152076926+Kingrane@users.noreply.github.com>
2025-06-09 12:53:39 +0300

    Update 27.py

--- infEGE/ege/27.py
+++ infEGE/ege/27.py
@@ -1,222 +1,177 @@
-n = int(input())
-a = []
-for i in range(n):
-    a.append(int(input()))
-k = 0
-for i in range(n):
-    for j in range(i+1, n):
-        s = a[i] + a[j]
-        if s % 7 == 0:
-            k += 1
-print(k)
-###########################################
-# Контрольная сумма R
-n = int(input())
-a = []
-for i in range(n):
-    a.append(int(input()))
-r = int(input())
-v = 0
-for i in range(n):
-    for j in range(i+1, n):
-        p = a[i] * a[j]
-        if p % 14 == 0 and p > v:
-            v = p
-print(v)
-if r == v:
-    print('yes')
-######## датчик, 15 секунд, мин. нечет. произведение
-n = int(input())
-a = []
-for i in range(n):
-    a.append(int(input()))
+# вот
 
-min = 999999999999999999
-for i in range(n):
-    for j in range(i+15, n):
-        p = a[i] * a[j]
-        if p % 2 != 0 and p < min:
-            min = p
-if min == 999999999999999999:
-    min = -1
-print(min)
-######### Менее 5 расстояние
-n = int(input())
-a = []
-for i in range(n):
-    a.append(int(input()))
-k = 0
-for i in range(n):
-    for j in range(i+1, min(i+5, n)):
-        if a[i] * a[j] % 14 == 0:
-            k += 1
-print(k)
-######### ai > aj, максимальная сумма температур m = 244, k = 20
-n = int(input())
-a = []
-for i in range(n):
-    a.append(int(input()))
-max = -1
-for i in range(n):
-    for j in range(i+1, n):
-        if a[i] > a[j]:
-            s = a[i] + a[j]
-            if s % 244 = 20 and s > max:
-                max = s
-                a = a[i]
-                b = a[j]
-print(a, b)
-#############
-n = int(input())
-a = []
-for i in range(n):
-    a.append(int(input()))
-max = -1
-for i in range(n):
-    for j in range(i+1, n):
-        for p in range(j+1, n):
-            s = a[i] + a[j] + a[p]
-print(a, b)
-##################################### минимальная сумма из всех пар и не делится
-f = open('27.txt')
-n = int(f.readline())
-a = []
-for i in range(n):
-    b, c = map(int, f.readline().split())
-    a.append([b,c])
-min = 9999999999
-for v0 in a[0]:
-    for v1 a[1]:
-        for v2 in a[2]:
-            for v3 in a[3]:
-                for v4 in a[4]:
-                    for v5 in a[5]:
-                        s = v0 + v1 + v2 + v3 + v4 + v5
-                        if s % 3 != 0 and s < min:
-                            min = s
-print(min)
-######################### непрерывные подпоследовательностей сумма кратна 999
-f = open('27.txt')
-n = int(f.readline())
-a = []
-for i in range(n):
-    a.append(int(f.readline()))
-k = 0
-for i in range(n):
-    for j in range(i, n):
-        s = sum(a[i:j+1])
-        if s % 999:
-            k += 1
-print(k)
-##########################
-=======
-n = int(input())
-a = []
-for i in range(n):
-    a.append(int(input()))
-k = 0
-for i in range(n):
-    for j in range(i+1, n):
-        s = a[i] + a[j]
-        if s % 7 == 0:
-            k += 1
-print(k)
-#################### Контрольная сумма R
-n = int(input())
-a = []
-for i in range(n):
-    a.append(int(input()))
-r = int(input())
-v = 0
-for i in range(n):
-    for j in range(i+1, n):
-        p = a[i] * a[j]
-        if p % 14 == 0 and p > v:
-            v = p
-print(v)
-if r == v:
-    print('yes')
-######## датчик, 15 секунд, мин. нечет. произведение
-n = int(input())
-a = []
-for i in range(n):
-    a.append(int(input()))
+A = [[], []]
+B = [[], [], []]
 
-min = 999999999999999999
-for i in range(n):
-    for j in range(i+15, n):
-        p = a[i] * a[j]
-        if p % 2 != 0 and p < min:
-            min = p
-if min == 999999999999999999:
-    min = -1
-print(min)
-######### Менее 5 расстояние
-n = int(input())
-a = []
-for i in range(n):
-    a.append(int(input()))
-k = 0
-for i in range(n):
-    for j in range(i+1, min(i+5, n)):
-        if a[i] * a[j] % 14 == 0:
-            k += 1
-print(k)
-######### ai > aj, максимальная сумма температур m = 244, k = 20
-n = int(input())
-a = []
-for i in range(n):
-    a.append(int(input()))
-max = -1
-for i in range(n):
-    for j in range(i+1, n):
-        if a[i] > a[j]:
-            s = a[i] + a[j]
-            if s % 244 = 20 and s > max:
-                max = s
-                a = a[i]
-                b = a[j]
-print(a, b)
-#############
-n = int(input())
-a = []
-for i in range(n):
-    a.append(int(input()))
-max = -1
-for i in range(n):
-    for j in range(i+1, n):
-        for p in range(j+1, n):
-            s = a[i] + a[j] + a[p]
-print(a, b)
-##################################### минимальная сумма из всех пар и не делится
-f = open('27.txt')
-n = int(f.readline())
-a = []
-for i in range(n):
-    b, c = map(int, f.readline().split())
-    a.append([b,c])
-min = 9999999999
-for v0 in a[0]:
-    for v1 a[1]:
-        for v2 in a[2]:
-            for v3 in a[3]:
-                for v4 in a[4]:
-                    for v5 in a[5]:
-                        s = v0 + v1 + v2 + v3 + v4 + v5
-                        if s % 3 != 0 and s < min:
-                            min = s
-print(min)
-######################### непрерывные подпоследовательностей сумма кратна 999
-f = open('27.txt')
-n = int(f.readline())
-a = []
-for i in range(n):
-    a.append(int(f.readline()))
-k = 0
-for i in range(n):
-    for j in range(i, n):
-        s = sum(a[i:j+1])
-        if s % 999:
-            k += 1
-print(k)
-##########################
+for line in open("ege/27files/27-51a.txt"):
+    x, y = [float(i) for i in line.replace(',', '.').split()]
+    if x < 4:
+        A[0].append([x, y])
+    elif x > 7:
+        A[1].append([x, y])
+
+for line in open("ege/27files/27-51b.txt"):
+    x, y = [float(i) for i in line.replace(',', '.').split()]
+    if (8.8 > x > 3) and (y <= 7.5):
+        B[0].append([x, y])
+    elif (y >= 8.2) and (14 > x > 5):
+        B[1].append([x, y])
+    elif (14.8 >= x > 10) and (y < 6):
+        B[2].append([x, y])
+
+
+def d(a, b):
+    x1, y1 = a
+    x2, y2 = b
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
+
+def centers(cl):
+    m = []
+    for p in cl:
+        sm = sum(d(p, p1) for p1 in cl)
+        m.append([sm, p])
+    return min(m)[1]
+
+
+centerA = [centers(cl) for cl in A]
+centerB = [centers(cl) for cl in B]
+pxa = int(sum(x for x, y in centerA) / 2 * 100000)
+pya = int(sum(y for x, y in centerA) / 2 * 100000)
+pxb = int(sum(x for x, y in centerB) / 3 * 100000)
+pyb = int(sum(y for x, y in centerB) / 3 * 100000)
+print(pxa, pya)
+print(pxb, pyb)
+# -------------------------------------------------------
+# -------------------------------------------------------
+# -------------------------------------------------------
+A = [[], []]
+B = [[], [], []]
+
+for line in open("A.txt"):
+    x, y = [float(i) for i in line.split()]
+    if y < 3:
+        A[0].append([x, y])
+    else:
+        A[1].append([x, y])
+
+for line in open("B.txt"):
+    x, y = [float(i) for i in line.split()]
+    if y < -x + 8:
+        B[0].append([x, y])
+    elif y > 1.5 * x - 6.5:
+        B[1].append([x, y])
+    else:
+        B[2].append([x, y])
+
+
+def d(a, b):
+    x1, y1 = a
+    x2, y2 = b
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+
+def center(cl):
+    m = []
+    for p in cl:
+        sm = sum(d(p, p1) for p1 in cl)
+        m.append([sm, p])
+    return min(m)[1]
+
+
+centerA = [center(cl) for cl in A]
+centerB = [center(cl) for cl in B]
+pxa = int(sum(x for x, y in centerA) / 2 * 100000)
+pya = int(sum(y for x, y in centerA) / 2 * 100000)
+pxb = int(sum(x for x, y in centerA) / 3 * 100000)
+pyb = int(sum(y for x, y in centerA) / 3 * 100000)
+print(pxa, pya)
+print(pxb, pyb)
+
+# -------------------------------------------------------
+# -------------------------------------------------------
+# ----------------------+высота--------------------------
+
+A = [[], []]
+B = [[], [], []]
+
+for line in open("ege/27files/27-26a.txt"):
+    x, y, h = [float(i) for i in line.replace(',', '.').split()]
+    if x < 200:
+        A[0].append([x, y, h])
+    if x > 300:
+        A[1].append([x, y, h])
+
+for line in open("ege/27files/27-26b.txt"):
+    x, y, h = [float(i) for i in line.replace(',', '.').split()]
+    if (x < -50) and (y < -50):
+        B[0].append([x, y, h])
+    if (x < 0) and (x > -100) and (y > 0):
+        B[1].append([x, y, h])
+    if (x > 100) and (x < 200) and (y > -100):
+        B[2].append([x, y, h])
+
+
+def d(a, b):
+    x1, y1 = a[0], a[1]
+    x2, y2 = b[0], b[1]
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
+
+def center(cl):
+    m = []
+    for p in cl:
+        sm = sum(d(p, p1) * p1[2] for p1 in cl)
+        m.append([sm, p])
+    return min(m)[1]
+
+
+cA = [center(cl) for cl in A]
+cB = [center(cl) for cl in B]
+pxa = int(sum(p[0] for p in cA) / 2 * 100000)
+pya = int(sum(p[1] for p in cA) / 2 * 100000)
+pxb = int(sum(p[0] for p in cB) / 3 * 100000)
+pyb = int(sum(p[1] for p in cB) / 3 * 100000)
+print(pxa, pya)
+print(pxb, pyb)
+
+# -------------------------------------------------------
+# -------------------------------------------------------
+# -------------------------------------------------------
+
+data = []
+for line in open("ege/27files/27_A_21931.txt"):
+    x, y = [float(i) for i in line.replace(',', '.').split()]
+    data.append([x, y])
+
+
+def d(a, b):
+    x1, y1 = a
+    x2, y2 = b
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+
+def center(cl):
+    m = []
+    for p in cl:
+        sm = sum(d(p, p1) for p1 in cl)
+        m.append([sm, p])
+    return min(m)[1]
+
+
+clusters = []
+while data:
+    cl = [data.pop()]
+    for p in cl:
+        sosed = [p1 for p1 in data if d(p, p1) < 1]
+        for p1 in sosed:
+            cl.append(p1)
+            data.remove(p1)
+    clusters.append(cl)
+
+print([len(cl) for cl in clusters])
+
+c = [center(cl) for cl in clusters]
+px = int(sum(x for x, y in c) / len(c) * 10000)
+py = int(sum(y for x, y in c) / len(c) * 10000)
+print(px, py)
